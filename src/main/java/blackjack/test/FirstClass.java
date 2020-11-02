228d0398102933107ee0ed2dd8aa832abf5e4c03
Patryk Ostachowicz <ostach98@vp.pl>
2020-11-02 20:48:19 +0100

    First test

---
+++ src/main/java/blackjack/test/FirstClass.java
@@ -0,0 +1,4 @@
+package blackjack.test;
+
+public class FirstClass {
+}
---
+++ src/test/java/blackjack/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package blackjack.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
